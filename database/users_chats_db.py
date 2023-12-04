f18e3b3a2b588d31a2042973fdb18a13c7800144
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2023-12-03 22:16:55 -0800

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -15,6 +15,9 @@ class Database:
         return dict(
             id = id,
             name = name,
+            _id=int(id),                                   
+            file_id=None,
+            caption=None,
             ban_status=dict(
                 is_banned=False,
                 ban_reason="",
@@ -163,5 +166,25 @@ class Database:
 
     async def get_db_size(self):
         return (await self.db.command("dbstats"))['dataSize']
+
+    async def set_thumbnail(self, id, file_id):
+        await self.col.update_one({'id': int(id)}, {'$set': {'file_id': file_id}})
+
+    async def get_thumbnail(self, id):
+        try:
+            thumbnail = await self.col.find_one({'id': int(id)})
+            if thumbnail:
+                return thumbnail.get('file_id')
+            else:
+                return None
+        except Exception as e:
+            print(e)
+
+    async def set_caption(self, id, caption):
+        await self.col.update_one({'id': int(id)}, {'$set': {'caption': caption}})
+
+    async def get_caption(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('caption', None)
 
 db = Database(DATABASE_URI, DATABASE_NAME)
