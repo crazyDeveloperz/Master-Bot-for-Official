e1cdca864c2e24ca81bd7616d083fdae1f52d4d4
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2023-12-03 22:24:09 -0800

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -12,7 +12,7 @@ import pyrogram
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
     make_inactive
 from info import *
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto, ForceReply #ForceReply for rname
 from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
 from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings, get_shortlink, send_all, check_verification, get_token
@@ -32,11 +32,119 @@ import logging
 from urllib.parse import quote_plus
 from util.file_properties import get_name, get_hash, get_media_file_size
 
+from database.crazy_utils import progress_for_pyrogram, convert, humanbytes
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+import os 
+import humanize
+from PIL import Image
+import time
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
 BUTTONS = {}
 SPELL_CHECK = {}
+
+@Client.on_callback_query(filters.regex('rename'))
+async def rename(bot,update):
+	user_id = update.message.chat.id
+	date = update.message.date
+	await update.message.delete()
+	await update.message.reply_text("»»——— 𝙋𝙡𝙚𝙖𝙨𝙚 𝙚𝙣𝙩𝙚𝙧 𝙣𝙚𝙬 𝙛𝙞𝙡𝙚 𝙣𝙖𝙢𝙚...",	
+	reply_to_message_id=update.message.reply_to_message.id,  
+	reply_markup=ForceReply(True))  
+# Born to make history @LazyDeveloper !
+@Client.on_callback_query(filters.regex("upload"))
+async def doc(bot, update):
+    type = update.data.split("_")[1]
+    new_name = update.message.text
+    new_filename = new_name.split(":-")[1]
+    file = update.message.reply_to_message
+    file_path = f"downloads/{new_filename}"
+    ms = await update.message.edit("\n𝙏𝙧𝙮𝙞𝙣𝙜 𝙩𝙤 𝙙𝙤𝙬𝙣𝙡𝙤𝙖𝙙...ღ")
+    c_time = time.time()
+    try:
+        path = await bot.download_media(
+                message=file,
+                progress=progress_for_pyrogram,
+                progress_args=("**\n  𝙏𝙧𝙮𝙞𝙣𝙜 𝙩𝙤 𝙙𝙤𝙬𝙣𝙡𝙤𝙖𝙙𝙞𝙣𝙜...ღ", ms, c_time))
+    except Exception as e:
+        await ms.edit(e)
+        return 
+    splitpath = path.split("/downloads/")
+    dow_file_name = splitpath[1]
+    old_file_name =f"downloads/{dow_file_name}"
+    os.rename(old_file_name, file_path)
+    duration = 0
+    try:
+        metadata = extractMetadata(createParser(file_path))
+        if metadata.has("duration"):
+           duration = metadata.get('duration').seconds
+    except:
+        pass
+    user_id = int(update.message.chat.id) 
+    ph_path = None 
+    media = getattr(file, file.media.value)
+    filesize = humanize.naturalsize(media.file_size) 
+    c_caption = await db.get_caption(update.message.chat.id)
+    c_thumb = await db.get_thumbnail(update.message.chat.id)
+    if c_caption:
+         try:
+             caption = c_caption.format(filename=new_filename, filesize=humanize.naturalsize(media.file_size), duration=convert(duration))
+         except Exception as e:
+             await ms.edit(text=f"Your caption Error unexpected keyword ●> ({e})")
+             return 
+    else:
+        caption = f"**📁 𝐅𝐢𝐥𝐞 𝐧𝐚𝐦𝐞 :- {new_filename} \n\n🗃️ 𝙁𝙞𝙡𝙚 𝙨𝙞𝙯𝙚 :- {filesize}**"
+    if (media.thumbs or c_thumb):
+        if c_thumb:
+           ph_path = await bot.download_media(c_thumb) 
+        else:
+           ph_path = await bot.download_media(media.thumbs[0].file_id)
+        Image.open(ph_path).convert("RGB").save(ph_path)
+        img = Image.open(ph_path)
+        img.resize((320, 320))
+        img.save(ph_path, "JPEG")
+    await ms.edit("**𝘗𝘳𝘦𝘱𝘢𝘳𝘪𝘯𝘨 𝘵𝘰 𝘳𝘦𝘤𝘦𝘪𝘷𝘦 𝘧𝘪𝘭𝘦...︻デ═一**")
+    c_time = time.time() 
+    try:
+       if type == "document":
+          await bot.send_document(
+	        update.message.chat.id,
+                   document=file_path,
+                   thumb=ph_path, 
+                   caption=caption, 
+                   progress=progress_for_pyrogram,
+                   progress_args=( "**✧ ᴜᴘʟᴏᴀᴅɪɴɢ ꜰɪʟᴇ ꜰʀᴏᴍ ꜱᴇʀᴠᴇʀ ✧**",  ms, c_time))
+       elif type == "video": 
+           await bot.send_video(
+	        update.message.chat.id,
+	        video=file_path,
+	        caption=caption,
+	        thumb=ph_path,
+	        duration=duration,
+	        progress=progress_for_pyrogram,
+	        progress_args=( "**✧ ᴜᴘʟᴏᴀᴅɪɴɢ ꜰɪʟᴇ ꜰʀᴏᴍ ꜱᴇʀᴠᴇʀ ✧**",  ms, c_time))
+       elif type == "audio": 
+           await bot.send_audio(
+	        update.message.chat.id,
+	        audio=file_path,
+	        caption=caption,
+	        thumb=ph_path,
+	        duration=duration,
+	        progress=progress_for_pyrogram,
+	        progress_args=( "**✧ ᴜᴘʟᴏᴀᴅɪɴɢ ꜰɪʟᴇ ꜰʀᴏᴍ ꜱᴇʀᴠᴇʀ ✧**",  ms, c_time   )) 
+    except Exception as e: 
+        await ms.edit(f" Erro {e}") 
+        os.remove(file_path)
+        if ph_path:
+          os.remove(ph_path)
+        return 
+    await ms.delete() 
+    os.remove(file_path) 
+    if ph_path:
+       os.remove(ph_path)
 
 
 @Client.on_message(filters.text & filters.incoming)
